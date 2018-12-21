91bc0879cd7ef03f0d58c236b0d5d9b0b99daa5a
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2018-12-21 23:33:03 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import subprocess
 import os
+import sys
+from pathlib import Path
 print('initing...')
 # load
-print('all right!')
-# main
+my_file = Path('C:\htzqzyb2\hexin.exe')
+if my_file.exists():
+    child = subprocess.Popen('C:\htzqzyb2\hexin.exe',shell=True)
+    child.wait()
+    child = subprocess.Popen('cd C:\htzqzyb2',shell=True)
+    child.wait()
+    child = subprocess.Popen('git add .',shell=True)
+    child.wait()
+    child = subprocess.Popen('git commit -m update',shell=True)
+    child.wait()
+    child = subprocess.Popen('git push orgin',shell=True)
+else:
+    child = subprocess.Popen('cd C:\ ')
